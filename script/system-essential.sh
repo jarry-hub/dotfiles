3d9be7ef099106e8349bff6b910170ab4ba9a6ca
chengyi818 <chengyi818@foxmail.com>
2016-01-31 09:33:47 +0800

    update nessenary

--- script/system-essential.sh
+++ script/system-essential.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #自动安装zsh及ohmyzsh环境
-sudo apt-get install tmux zsh git wget
+sudo apt-get install tmux zsh git wget cmake build-essential python-dev
 if [ ! -d ~/.oh-my-zsh ]; then
     echo "oh my zsh didn't exist,download......."
     wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
