3c2a99ced1029f9191e63c555d4c5871fabd42d4
chengyi818 <chengyi818@foxmail.com>
2016-03-07 12:28:15 +0800

    update

--- install.sh
+++ install.sh
@@ -22,8 +22,8 @@ if [ ! -d ~/.oh-my-zsh ]; then
     wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 fi
 
-sudo bash script/system-essential.sh
-sudo bash script/symlink-dotfiles.sh
+sudo bash dot_script/system-essential.sh
+sudo bash dot_script/symlink-dotfiles.sh
 
 #Install vimrc framework spf13
 curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
