cd8a5a44c95e0125bb2d78f02b3fe34de1432929
chengyi818 <chengyi818@foxmail.com>
2016-03-07 13:21:41 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,14 @@ fi
 mkdir -p $dir
 cd $dir
 
-sudo bash dot_script/system-essential.sh
+#Install some essential package
+if [ -f /usr/bin/apt-get ];then
+    sudo apt-get install tmux zsh git wget cmake build-essential python-dev ctags cscope
+else
+    echo "apt-get isn't in you system,so you need install some necessary package by you own."
+fi
+
+chsh -s /bin/zsh
 
 #Download my dotfiles
 git clone -b develop --recursive git://github.com/chengyi818/dotfiles.git
@@ -22,11 +29,42 @@ if [ ! -d ~/.oh-my-zsh ]; then
     wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 fi
 
-sudo bash dot_script/symlink-dotfiles.sh
+############################
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+echo $SCRIPTPATH
+
+dir=$SCRIPTPATH/..
+# old dotfiles backup directory
+olddir=$SCRIPTPATH/../dotfiles_old          
+# list of files/folders to symlink in homedir
+files="bashrc script zshrc tmux.conf ycm_extra_conf.py gitconfig vimrc.local vimrc.before.local vimrc.bundles.local"
+
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in current directory"
+mkdir -p $olddir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    echo "Backup ~/.$file from ~ to $olddir"
+    mv ~/.$file $olddir 2>/dev/null
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/config/$file ~/.$file
+done
 
 #Install vimrc framework spf13
+cd $dir
 curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
-
 
 #At last source everything
 source ~/.bashrc
