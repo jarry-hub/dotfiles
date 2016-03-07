a1657d7f0dd0be7d80a7089efa9a69f2f8725e47
chengyi818 <chengyi818@foxmail.com>
2016-03-07 18:32:58 +0800

    如果没有下载YCM,就不编译

--- script_dot_use/YouCompleteMe.sh
+++ script_dot_use/YouCompleteMe.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-cd ~/.vim/bundle/YouCompleteMe
-git submodule update --init --recursive
-./install.py --clang-completer 
+if [ -d ~/.vim/bundle/YouCompleteMe ];then
+    cd ~/.vim/bundle/YouCompleteMe
+    git submodule update --init --recursive
+    ./install.py --clang-completer
+fi
