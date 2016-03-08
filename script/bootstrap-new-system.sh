4f6ff69f121754bf2c758b8319ed949ef81e8dde
chengyi818 <chengyi818@foxmail.com>
2016-03-08 19:34:59 +0800

    clean branch master

--- script/bootstrap-new-system.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#自动安装大量软件
-sudo apt-get install -y zsh wget tmux git vim gitk  python-pip python-dev  smplayer shutter libgoo-canvas-perl openvpn  minicom wireshark tftp-hpa  libnet-tftp-perl tftpd-hpa xinetd roxterm kde-full meld gcc g++  binutils  patch  bzip2 flex  bison  make autoconf gettext  texinfo  unzip sharutils subversion  libncurses5-dev  ncurses-term  zlib1g-dev gawk asciidoc libz-dev
-
-if [ ! -d "~/.oh-my-zsh" ]; then
-    wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
-fi
-chsh -s /bin/zsh
--- script/vim-plugin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-vim +PluginInstall
-killall  vim
-cd ~/.vim/bundle/YouCompleteMe
-git submodule update --init --recursive
-./install.py --clang-completer
-
