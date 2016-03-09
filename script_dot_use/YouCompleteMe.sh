0762cf655aa3ae4a008d82b3143a67db227f780b
chengyi818 <chengyi818@foxmail.com>
2016-03-09 08:53:57 +0800

    update YCM.sh

--- script_dot_use/YouCompleteMe.sh
+++ script_dot_use/YouCompleteMe.sh
@@ -1,11 +1,13 @@
 if [ -d ~/.vim/bundle/YouCompleteMe ];then
     if which apt-get >/dev/null; then
-        cd ~/.vim/bundle/YouCompleteMe
-        git submodule update --init --recursive
-        ./install.py --clang-completer
-        ret="$?"
-        if [ "$ret" -ne '0' ];then
-            echo "编译YouCompleteMe的过程中出错啦,少年,你可以换个补全插件或者自己手动编译YCM"
+        if ［ $（getconf WORD_BIT） = ‘32’ ］ && ［ $（getconf LONG_BIT） = ‘64’ ］ ; then
+            cd ~/.vim/bundle/YouCompleteMe
+            git submodule update --init --recursive
+            ./install.py --clang-completer
+            ret="$?"
+            if [ "$ret" -ne '0' ];then
+                echo "编译YouCompleteMe的过程中出错啦,少年,你可以换个补全插件或者自己手动编译YCM"
+            fi
         fi
     else
         echo "看起来,你必须自己手动编译YouCompleteME了"
